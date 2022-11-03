12c285aab0d7bcb5ff51aaa267f2230c60a9514a
iforsmann <iforsmann@frontier.com>
2022-11-02 20:05:19 -0700

    removed unused method

--- pyboids/app/utils.py
+++ pyboids/app/utils.py
@@ -76,6 +76,3 @@ def truncate(vector, max_length):
         return normalize(vector, pre_computed=n) * max_length
     else:
         return vector
-
-def tuple_distance(p1, p2):
-        return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
